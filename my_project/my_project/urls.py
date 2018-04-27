54dde8d19d055a1a083e2550b458a64bdab09b44
ashutosh tripathy <ashutosh_tripathy@outlook.com>
2018-04-27 14:01:40 +0530

    Fix url import issue

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
-from django.contrib import admin
+from django.conf.urls import url, include
+from rest_framework import routers
+from my_app import views
+#from django.core.urlresolvers import reverse
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
